19168d17c50168b5d7ca5df199bf43f39d066370
Silfester <s.csemy@gmail.com>
2020-04-26 19:50:37 +0200

    First

---
+++ pages/about.vue
@@ -0,0 +1,107 @@
+<template>
+  <!--==========================
+  About Section
+  ============================-->
+
+  <section id="about">
+
+    <div class="container wow fadeInUp">
+
+      <div class="row">
+
+        <div class="col-md-12">
+
+          <h3 class="section-title">O mne</h3>
+
+          <div class="section-title-divider"></div>
+
+          <p class="section-description"></p>
+
+        </div>
+
+      </div>
+
+    </div>
+
+    <div class="container about-container wow fadeInUp">
+
+      <div class="row">
+
+
+
+        <div class="col-lg-6 about-img">
+
+          <a href="lib/swipebox/img/cert-webrebel.jpg" class="swipebox" title="Certificate Webrebel (HTML & CSS & Javascript)">
+              <img src="lib/swipebox/img/cert-webrebel-min.jpg" alt="image-Certificate Webrebel">
+              </a>
+
+          <a href="lib/swipebox/img/cert-sass.jpg" class="swipebox" title="Certificate SASS">
+            <img src="lib/swipebox/img/cert-sass-min.jpg" alt="image-Certificate SASS">
+            </a>
+          
+            
+
+        </div>
+
+
+
+        <div class="col-lg-6 about-content">
+
+          <h2 class="about-title">Ponúkam profestionálne služby</h2>
+
+          <p class="about-text">
+
+            Ponúkam opravu a úpravu fotografií a tiež vytvorenie moderného a užívateľsky prívetíveho webu. 
+
+          </p>
+
+          <p class="about-text">
+
+            Úprave fotografií sa venujem už viac ako desať rokov.
+
+            Na prácu s fotkami používam špičkový Adobe Photoshop a profesionálne foto filtre.
+
+            
+
+          </p>
+
+          <p class="about-text">
+
+            Webstránky tvorím vo Visual Studiu použitím HTML, CSS, JavaScript a Bootstrapu. 
+
+            V prípade e-shopov je to Wordpress, ktorý dovoľuje jednoduché spravovanie samotným zákaznikom bez znalosti kódovania.
+
+          </p>
+
+          <p class="about-text">
+          </p>
+
+        </div>
+        <div class="col-lg-12 about-icons">
+        <ul>
+           <li><i class="fab fa-html5"></i></li>
+           <li><i class="fab fa-css3"></i></li>
+           <li><i class="fab fa-js-square"></i></li>
+           <li><i class="fab fa-sass"></i></li>
+           <li><i class="fab fa-bootstrap"></i></li>
+           
+      </ul>
+    </div>
+      </div>
+
+    </div>
+
+  </section>
+
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
