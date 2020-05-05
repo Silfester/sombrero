567594a93e6b2e45cc38a2b280cbb73efee7b83d
Silfester <s.csemy@gmail.com>
2020-05-05 18:37:31 +0200

    imnotsure

--- pages/index.vue
+++ pages/index.vue
@@ -15,9 +15,16 @@
 
 
         <h1>Vitajte na mojej stránke</h1>
-
-        <h2>Tvorím <span class="rotating">kvalitné fotografie,moderné web stránky</span></h2>
-
+<div id="wordcar">
+        <h2 class="wordCarousel">
+          <span>Tvorím</span> 
+          <div>
+            <ul class="flip2"> 
+          <li>moderné webstránky</li>
+          <li>kvalitné fotografie</li>
+</ul>
+     </div></h2>
+</div>
         <div class="actions">
 
           <a href="#services" class="btn-get-started">Foto</a>
@@ -1430,6 +1437,79 @@ body.mobile-nav-active #mobile-nav-toggle {
   width: 300px;
   height: 300px;
 }
+/* -------------------------------------------------------------- 
+ * ## text rotator
+ * -------------------------------------------------------------- */
+.wordCarousel {
+    font-size: 36px;
+    font-weight: 100;
+    color: #939393;
+}
+.wordCarousel div {
+        overflow: hidden;
+        position: relative;
+        float: right;
+        height: 65px;
+        padding-top: 10px;
+        margin-top: -10px;}
+.wordCarousel li {
+ 
+            color: #ccc;
 
+            padding: 0 10px;
+            height: 45px;
+            margin-bottom: 45px;
+            display: block;
+            padding: 0;
+            text-align: left;
+        }
+.wordCarousel ul {
+            padding-left: 10px ;
+        }
+
+
+.flip2 { animation: flip2 6s cubic-bezier(0.23, 1, 0.32, 1.2) infinite; }
+.flip3 { animation: flip3 8s cubic-bezier(0.23, 1, 0.32, 1.2) infinite; }
+.flip4 { animation: flip4 10s cubic-bezier(0.23, 1, 0.32, 1.2) infinite; }
+
+@keyframes flip2 {
+    0% { margin-top: -180px; }
+    5% { margin-top: -90px;  }
+    50% { margin-top: -90px; }
+    55% { margin-top: 0px; }
+    99.99% { margin-top: 0px; }
+    100% { margin-top: -270px; }
+}
+
+@keyframes flip3 {
+    0% { margin-top: -270px; }
+    5% { margin-top: -180px; }
+    33% { margin-top: -180px; }
+    38% { margin-top: -90px; }
+    66% { margin-top: -90px; }
+    71% { margin-top: 0px; }
+    99.99% { margin-top: 0px; }
+    100% { margin-top: -270px; }
+}
+
+@keyframes flip4 {
+    0% { margin-top: -360px; }
+    5% { margin-top: -270px; }
+    25% { margin-top: -270px; }
+    30% { margin-top: -180px; }
+    50% { margin-top: -180px; }
+    55% { margin-top: -90px; }
+    75% { margin-top: -90px; }
+    80% { margin-top: 0px; }
+    99.99% { margin-top: 0px; }
+    100% { margin-top: -270px; }
+}
+
+#wordcar {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
+}
 
 </style>
